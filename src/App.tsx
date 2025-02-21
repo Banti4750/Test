8af6a36f73cfe30b3351b509d7ea499cec1715bf
Banti4750 <bantikumar6203818460@gmail.com>
2025-02-21 07:38:15 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-500'>aban</div>
+      <div className="bg-black h-screen w-full flex justify-center items-center ">
+        <h1 className="text-white text-9xl">Banti Kumar</h1>
+      </div>
     </>
   )
 }
